9545922f3d8ce601af30ca0b3dad2059dbfb3f83
gitb890 <2832816260@qq.com>
2022-10-11 21:50:48 +0800

    集合操作

---
+++ drew/demo11.py
@@ -0,0 +1,15 @@
+empty_set = set()
+print(empty_set)
+
+# 求集合的交集
+s={1,2,3,4,5}
+s1 = {5,6,7,8,9,10}
+print(s&s1)
+print(s.intersection(s1))
+# 求集合的并集
+print(s|s1)
+# 求集合的差集
+print(s-s1)
+print(s1-s)
+print(s.intersection(s1))
+
